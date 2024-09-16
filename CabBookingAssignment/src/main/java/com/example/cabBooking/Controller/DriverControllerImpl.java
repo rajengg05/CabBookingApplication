5bfa4a5b755dbd35979b8afb0b6aae8825dc2524
rajengg05 <90300744+rajengg05@users.noreply.github.com>
2024-09-17 03:21:15 +0530

    Made more changes in exception handling code

--- CabBookingAssignment/src/main/java/com/example/cabBooking/Controller/DriverControllerImpl.java
+++ CabBookingAssignment/src/main/java/com/example/cabBooking/Controller/DriverControllerImpl.java
@@ -1,5 +1,6 @@
 package main.java.com.example.cabBooking.Controller;
 
+import main.java.com.example.cabBooking.Exceptions.DuplicateDriverException;
 import main.java.com.example.cabBooking.Models.Driver;
 import main.java.com.example.cabBooking.Models.Location;
 
@@ -8,16 +9,21 @@ import java.util.List;
 
 public class DriverControllerImpl implements DriverController{
     private List<Driver> drivers;
-
+    private List<String> driverNames;
     public DriverControllerImpl(){
       drivers= new ArrayList<>();
+      driverNames= new ArrayList<>();
     }
     @Override
     public void addDriver(String driverDetails, String vehicleDetails, Location location) {
         String[] details = driverDetails.split(",");
         String[] vehicle = vehicleDetails.split(",");
+        if(driverNames.contains(details[0].trim())){
+            throw new DuplicateDriverException("Driver already exists");
+        }
         drivers.add(new Driver(details[0].trim(), details[1].trim(), Integer.parseInt(details[2].trim()),
                 vehicle[0].trim(), vehicle[1].trim(), location));
+        driverNames.add(details[0].trim());
         System.out.println("Driver " + details[0] + " added.");
     }
 
--- CabBookingAssignment/src/main/java/com/example/cabBooking/Controller/UserControllerImpl.java
+++ CabBookingAssignment/src/main/java/com/example/cabBooking/Controller/UserControllerImpl.java
@@ -1,5 +1,6 @@
 package main.java.com.example.cabBooking.Controller;
 
+import main.java.com.example.cabBooking.Exceptions.DuplicateUserException;
 import main.java.com.example.cabBooking.Models.User;
 
 import java.util.ArrayList;
@@ -7,14 +8,19 @@ import java.util.List;
 
 public class UserControllerImpl implements UserController{
     private List<User> users;
-
+    private List<String> userNames;
     public UserControllerImpl(){
         users= new ArrayList<>();
+        userNames=new ArrayList<>();
     }
     @Override
     public void addUser(String userDetails) {
         String[] details = userDetails.split(",");
+        if(userNames.contains(details[0].trim())){
+            throw new DuplicateUserException("User already exists");
+        }
         users.add(new User(details[0].trim(), details[1].trim(), Integer.parseInt(details[2].trim())));
+        userNames.add(details[0].trim());
         System.out.println("User " + details[0] + " added.");
     }
 }
